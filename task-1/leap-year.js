777de70212e1cabf7ba2270d1575e476d640d695
Shadi <sh.moonwalker7@gmail.com>
2026-01-20 21:38:41 +0100

    implement detecting leap year logic with user input validation

--- task-1/leap-year.js
+++ task-1/leap-year.js
@@ -2,8 +2,27 @@ import promptSync from 'prompt-sync';
 const prompt = promptSync();
 
 
-// Write your code here
-// Guidance:
-// Step 1: prompt the user to enter a year
-// Step 2: convert the user input to a number so we can perform calculations
-// Step 3: Implement the logic
+const year = Number(prompt("Enter a year: "));
+
+if (isNaN(year) || year === "") {
+    console.log("Not a Valid Year.Plese Tryy Again");
+
+}
+
+if (year <= 1 && year >= 9999) {
+    console.log("Not a Valid Year.Plese Tryy Again");
+}
+
+else {
+    if (year % 4 === 0) {
+        if (year % 100 === 0) {
+            if (year % 400 === 0) {
+                console.log("Yes, " + year + " is a leap year");
+            }
+            else console.log("No, " + year + " is not a leap year");
+
+        }
+        else console.log("Yes, " + year + " is a leap year");
+    }
+    else console.log("No, " + year + " is not a leap year");
+}
