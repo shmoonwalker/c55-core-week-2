d706739f63f31504aec24f16d685a28c20a8116c
Shadi <114850111+shmoonwalker@users.noreply.github.com>
2026-01-29 20:48:28 +0100

    Refactor login function for better error handling

--- task-2/login.js
+++ task-2/login.js
@@ -6,24 +6,26 @@ let isBlock = false;
 
 function onLogin(username, password) {
   if (isBlock) {
-    errorMessage("Login blocked: Too many incorrect attempts")
+    errorMessage("Login blocked: Too many incorrect attempts");
+    return;
   }
-  else {
-    if ((username === "admin" && password === "Hack1234") || (username === "user" && password === "7654321")) {
-      successMessage("Logged in successfully")
-    }
-    else if ((username === "admin" && password != "Hack1234") || (username === "user" && password != "7654321")) {
-      incorrectAttempts++;
-      errorMessage("Incorrect credentials")
-    }
-    else {
-      errorMessage("Incorrect credentials")
-    }
-    if (incorrectAttempts > 3) {
+
+  if (
+    (username === "admin" && password === "Hack1234") ||
+    (username === "user" && password === "7654321")
+  ) {
+    successMessage("Logged in successfully");
+  } else {
+    incorrectAttempts++
+
+  
+    if (incorrectAttempts >= 4) {
       isBlock = true;
+      errorMessage("Login blocked: Too many incorrect attempts");
+    } else {
+      errorMessage("Incorrect credentials");
     }
   }
 }
 
-// Do not change the line below
 export { onLogin };
